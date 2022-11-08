65f5b3b14e3b3a7119fc6350c12ad31ea71763fa
Samarth <samdhawan8765@gmail.com>
2022-11-08 19:17:06 +0530

    integrated joke api and added README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dependencies
+ - pytelegrambotapi
+ - requests
+ - dotenv (for .env files)
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import telebot
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
@@ -11,4 +12,21 @@ bot = telebot.TeleBot(API_KEY,parse_mode=None)
 def send_message(message):
     bot.reply_to(message,'Aur bhau ka ba?')
 
-bot.polling()
+@bot.message_handler(commands=['hello'])
+def hello(message):
+    name = message.from_user.first_name
+    msg = f'Hello {name}'
+    bot.send_message(message.chat.id,msg)
+
+@bot.message_handler(commands=['joke'])
+def send_joke(message):
+    response = requests.get('https://v2.jokeapi.dev/joke/Dark,Programming,Miscellaneous,Pun,Spooky?')
+    res = response.json()
+    if(res['type'] == 'single'):
+        msg = res['joke']
+    else:
+       msg =f"Setup-> {res['setup']} \nDelivery->{res['delivery']}"
+
+    bot.send_message(message.chat.id,msg)
+
+bot.infinity_polling()
